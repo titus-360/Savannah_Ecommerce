6db996ad95505b1dd22e97741676e69e7861a136
titus-360 <murithititus510@gmail.com>
2025-06-01 18:13:09 +0300

    Configure ci_settings to use dj-database-url and add to requirements

--- savannah_ecommerce/ci_settings.py
+++ savannah_ecommerce/ci_settings.py
@@ -70,11 +70,10 @@ WSGI_APPLICATION = 'savannah_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 # Password validation
